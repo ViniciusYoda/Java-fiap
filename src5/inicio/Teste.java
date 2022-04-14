8a696d96b8e35566c56244bcd08480946a4b67fe
ViníciusYoda <rm92969@fiap.com.br>
2022-04-14 16:54:20 -0300

    Java

---
+++ src5/inicio/Teste.java
@@ -0,0 +1,18 @@
+package inicio;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Oi mundo");
+		System.out.println("Teste para comentário");
+		System.out.println("erro 1");
+		System.out.println("erro 2");
+		System.out.println("erro 3");
+		System.out.println("erro 4");
+		System.out.println("erro 5");
+		//System.out.println("erro 6");
+		
+	}
+
+}
