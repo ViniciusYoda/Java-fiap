151703526c05edc82f1509196461591026857091
Logon Aluno <90533635+ViniciusYoda@users.noreply.github.com>
2022-04-25 10:29:09 -0300

    construtores colocados

--- construtores/src/construtores/Caixa.java
+++ construtores/src/construtores/Caixa.java
@@ -6,5 +6,12 @@ public class Caixa {
 	private double altura;
 	private double profundidade;
 	
+	//construtor
+	public Caixa() {
+		System.out.println("Construção da Caixa");
+		this.largura = 10;
+		this.altura = 10;
+		this.profundidade = 10;
+	}
 
 }
