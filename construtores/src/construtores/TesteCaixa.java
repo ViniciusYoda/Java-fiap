0d28d81a9cd4eb4b01efe4e52953a8177892e635
Logon Aluno <90533635+ViniciusYoda@users.noreply.github.com>
2022-04-25 10:40:15 -0300

    testando os getters

--- construtores/src/construtores/TesteCaixa.java
+++ construtores/src/construtores/TesteCaixa.java
@@ -2,7 +2,11 @@ package construtores;
 
 public class TesteCaixa {
 	public static void main(String[] args) {
-		
+		Caixa c1 = new Caixa();
+		System.out.println(c1);
+		System.out.println("Altura: " + c1.getAltura());
+		System.out.println("Largura: " + c1.getLargura());
+		System.out.println("Profundidade: " + c1.getProfundidade());
 	}
 
 }
