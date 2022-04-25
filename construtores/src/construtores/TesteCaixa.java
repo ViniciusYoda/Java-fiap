d533af87e9b35d22ab12d954b261b8fdb46127af
Logon Aluno <90533635+ViniciusYoda@users.noreply.github.com>
2022-04-25 10:57:32 -0300

    Caixa 2

--- construtores/src/construtores/TesteCaixa.java
+++ construtores/src/construtores/TesteCaixa.java
@@ -3,11 +3,12 @@ package construtores;
 public class TesteCaixa {
 	public static void main(String[] args) {
 		Caixa c1 = new Caixa();
-		System.out.println(c1);
-		System.out.println("Altura: " + c1.getAltura());
-		System.out.println("Largura: " + c1.getLargura());
-		System.out.println("Profundidade: " + c1.getProfundidade());
-		System.out.println("Volume: " + c1.volumeCaixa());
+		System.out.println("Volume c1: " + c1.volumeCaixa());
+		
+		System.out.println("================================");
+		Caixa c2 = new Caixa(10, 20, 15);
+		System.out.println("Volume c2: " + c2.volumeCaixa());
+		
 	}
 
 }
