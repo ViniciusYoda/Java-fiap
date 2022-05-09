0928462dcef2c728f78efd0fbd5556f68e3bcecd
Logon Aluno <90533635+ViniciusYoda@users.noreply.github.com>
2022-05-09 11:28:58 -0300

    fazer barulho

--- DDDplus/src/aulaDDD/Cachorro.java
+++ DDDplus/src/aulaDDD/Cachorro.java
@@ -4,6 +4,10 @@ public class Cachorro extends Animal{
 
 	public Cachorro(String e, String c, String h) {
 		super(e, c, h);
-		}
+	}
+	
+	public void fazerBarulho() {
+		System.out.println("Auau!");
+	}
 	
 }
---
+++ DDDplus/src/aulaDDD/Gato.java
@@ -0,0 +1,13 @@
+package aulaDDD;
+
+public class Gato extends Animal {
+
+	public Gato(String e, String c, String h) {
+		super(e, c, h);
+	}
+	
+	public void fazerBarulho() {
+		System.out.println("Miau!");
+	}
+	
+}
