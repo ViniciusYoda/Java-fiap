0ec5540a3b722da191e904b159e9ca38501ac420
ViníciusYoda <rm92969@fiap.com.br>
2022-05-09 19:27:53 -0300

    O teste deu certo

---
+++ TesteDoEclipse/src/TesteDoEclipse/TesteDoEclipse.java
@@ -0,0 +1,10 @@
+package TesteDoEclipse;
+
+public class TesteDoEclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá eclipse");
+	}
+
+}
