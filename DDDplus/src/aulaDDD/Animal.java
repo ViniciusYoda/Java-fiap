0905e349a869be84df29753775a9d78da9ad22a5
Logon Aluno <90533635+ViniciusYoda@users.noreply.github.com>
2022-05-09 11:21:21 -0300

    dddplus

--- DDDplus/src/aulaDDD/Animal.java
+++ DDDplus/src/aulaDDD/Animal.java
@@ -1,5 +1,19 @@
 package aulaDDD;
 
 public class Animal {
+	//atributos
+	private String especie;
+	private String cor;
+	private String habitat;
 	
+	//construtor
+	public Animal(String e, String c, String h) {
+		this.especie = e;
+		this.cor = c;
+		this.habitat = h;
+	}
+	
+	public void fazerBarulho() {
+		System.out.println("fazerBarulho");
+	}
 }
---
+++ DDDplus/src/aulaDDD/Cachorro.java
@@ -0,0 +1,9 @@
+package aulaDDD;
+
+public class Cachorro extends Animal{
+
+	public Cachorro(String e, String c, String h) {
+		super(e, c, h);
+		}
+	
+}
