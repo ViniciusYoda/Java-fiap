a7d6c07067f9370d7a980f2a21563ea1aa01b7d6
ViníciusYoda <rm92969@fiap.com.br>
2022-05-28 16:56:02 -0300

    Controle

---
+++ Reforço6/src/Aula06/ControleRemoto.java
@@ -0,0 +1,120 @@
+package Aula06;
+
+public class ControleRemoto implements Controlador{
+	//Atributos
+	private int volume;
+	private boolean ligado;
+	private boolean tocando;
+	
+	//Construtor
+	public ControleRemoto() {
+		this.setVolume(50); 
+		this.setLigado(true); 
+		this.setTocando(false);
+	}
+
+	private int getVolume() {
+		return volume;
+	}
+
+	private void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	private boolean isLigado() {
+		return ligado;
+	}
+
+	private void setLigado(boolean ligado) {
+		this.ligado = ligado;
+	}
+
+	private boolean isTocando() {
+		return tocando;
+	}
+
+	private void setTocando(boolean tocando) {
+		this.tocando = tocando;
+	}
+
+	@Override
+	public void ligar() {
+		this.setLigado(true);
+		
+	}
+
+	@Override
+	public void desligar() {
+		this.setLigado(false);
+	}
+
+	@Override
+	public void abrirMenu() {
+		System.out.println("========================================");
+		System.out.println("Está ligado? " + this.isLigado());
+		System.out.println("Está tocando? " + this.isTocando());
+		System.out.print("Volume: " + this.getVolume());
+		for (int i = 0; i <= this.getVolume(); i+=10) {
+			System.out.print("|");
+		}
+		System.out.println("");
+	}
+
+	@Override
+	public void fecharMenu() {
+		System.out.println("Fechando menu...");
+	}
+
+	@Override
+	public void maisVolume() {
+		if (this.isLigado()) {
+			this.setVolume(this.getVolume()+5);
+		}else {
+			System.out.println("Impossivel aumentar volume");
+		}
+	}
+
+	@Override
+	public void menosVolume() {
+		if (this.isLigado()) {
+			this.setVolume(this.getVolume()-5);
+		}else {
+			System.out.println("Impossivel diminuir volume");
+		}
+	}
+
+	@Override
+	public void ligarMudo() {
+		if (this.isLigado() && this.getVolume() > 0) {
+			this.setVolume(0);
+		}
+	}
+
+	@Override
+	public void desligarMudo() {
+		if (this.isLigado() && this.getVolume() == 0) {
+			this.setVolume(50);
+		}
+	}
+
+	@Override
+	public void play() {
+		if (this.isLigado() && !(this.isTocando())) {
+			this.setTocando(true);
+		}else {
+			System.out.println("Não conseguir reproduzir");
+		}
+	}
+
+	@Override
+	public void pause() {
+		if (this.isLigado() && this.isTocando()) {
+			this.setTocando(false);
+		}else {
+			System.out.println("Não conseguir pausar");
+		}
+	}
+	
+	
+	
+}
