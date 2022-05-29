e45d7f10623b80fbe203fca62eaba054965c637c
ViníciusYoda <rm92969@fiap.com.br>
2022-05-29 08:39:22 -0300

    lendo

---
+++ Reforço9/src/aula09/Publicacao.java
@@ -0,0 +1,9 @@
+package aula09;
+
+public interface Publicacao {
+	public void abrir();
+	public void fechar();
+	public void folhear(int p);
+	public void avancarPag();
+	public void voltarPag();
+}
