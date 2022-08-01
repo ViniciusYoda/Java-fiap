394cb6747da3cd944fcb7fce76f7ccf89baa09e8
Vinícius Yoda <vyoda4604@gmail.com>
2022-08-01 11:31:39 -0300

    Aula de revisão

---
+++ Aula14/src/Revisao/Exemplo1.java
@@ -0,0 +1,38 @@
+package Revisao;
+
+import java.util.Scanner;
+
+public class Exemplo1 {
+	
+	public int tamanhoVetor(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Tamanho: ");
+		int t = input.nextInt();
+		return t;
+	}
+	
+	public int[] criaVetor(int t) {
+		Scanner input = new Scanner(System.in);
+		int[] vetor  = new int[t];
+		for(int i = 0; i < vetor.length; i++){
+			System.out.println("Número: ");
+			vetor[i] = input.nextInt();
+		}
+		return vetor;
+	}
+	
+	public int entradaNumero(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Número: ");
+		int n = input.nextInt();
+		return n;
+	}
+	
+	public ? buscaElemento() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
