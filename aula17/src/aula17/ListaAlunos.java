082096576c0a0417fe33d7397ce374fb42139442
Vinícius Yoda <vyoda4604@gmail.com>
2022-08-15 11:11:23 -0300

    lista desalinos

--- aula17/src/aula17/ListaAlunos.java
+++ aula17/src/aula17/ListaAlunos.java
@@ -1,5 +1,18 @@
 package aula17;
 
 public class ListaAlunos {
+	private Aluno[] alunos = null;  // array de objetos
+	private int i = 0; // variavel de controle do indice do array
+	 
+	// metodo construtor
+	public ListaAlunos(int qtde){
+		// instaciando o array alunos
+		alunos = new Aluno[qtde];
+	}
 	
+	// insere um aluno na lista "alunos"
+	public void insereAluno(Aluno a) {
+		alunos[i] = a;
+		i++;
+	}
 }
