5d309a1f10f473271ec0b2ba110a27135d56e566
ViníciusYoda <rm92969@fiap.com.br>
2022-08-28 17:18:33 -0300

    Jogo da velha

---
+++ Matrizes/src/Campo.java
@@ -0,0 +1,16 @@
+public class Campo {
+	private char simbolo;
+	public Campo() {
+		this.simbolo=' ';
+	}
+	public char getSimbolo() {
+		return this.simbolo;
+	}
+	public void setSimbolo(char simbolo) {
+		if(this.simbolo==' ') {
+			this.simbolo=simbolo;
+		}else {
+			System.out.println("Campo já usando");
+		}
+	}
+}
