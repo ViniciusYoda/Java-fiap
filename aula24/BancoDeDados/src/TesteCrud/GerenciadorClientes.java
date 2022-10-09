de360798833485c0d1210dfe6bbbe15f2741d609
Vinícius Yoda <vyoda4604@gmail.com>
2022-10-09 12:03:57 -0300

    Conectado com o banco

--- aula24/BancoDeDados/src/TesteCrud/GerenciadorClientes.java
+++ aula24/BancoDeDados/src/TesteCrud/GerenciadorClientes.java
@@ -1,6 +1,7 @@
 package TesteCrud;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import oracle.jdbc.pool.OracleDataSource;
 
@@ -34,5 +35,19 @@ public class GerenciadorClientes {
         // Obter uma conexão com o SGBD
         conn = ods.getConnection();
         
-;    }
+    }
+
+    public void inserir(Cliente c) throws Exception{
+        String sql = "INSERT INTO persons(first_name, last_name) VALUES(?, ?)";
+
+        //Criando um Statement responsável por executar a instrução no BD
+        PreparedStatement ps = conn.prepareStatement(sql);
+
+        ps.setString(1, c.getNome());
+        ps.setString(2, c.getSobrenome());
+
+        ps.executeQuery();
+        
+        conn.close();
+    }
 }
