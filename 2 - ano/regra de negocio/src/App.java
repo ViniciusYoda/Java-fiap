81de5515b61b0f53aea155937545a1b38f6fb23d
Vinícius Yoda <vyoda4604@gmail.com>
2023-02-17 12:02:40 -0300

    java

--- 2 - ano/regra de negocio/src/App.java
+++ 2 - ano/regra de negocio/src/App.java
@@ -6,7 +6,7 @@ public class App {
         int contract = 0;
         String data;
         float value = 0;
-        int installments;
+        int installments = 0;
         Scanner number  = new Scanner(System.in);
         Scanner dataInput = new Scanner(System.in);
         Scanner valueInput = new Scanner(System.in);
@@ -21,6 +21,14 @@ public class App {
         value = valueInput.nextFloat();
         System.out.println("Enter number of instalments: ");
         installments = installmentsInput.nextInt();
+        float parcela = 0;
+        parcela = value / installments;
+        float parcela1 = parcela + (1/100) * 1 + (2/100);
+        float parcela2 = parcela + (1/100) * 2 + (2/100);
+        float parcela3 = parcela + (1/100) * 3 + (2/100);
+        System.out.println(parcela1);
+        System.out.println(parcela2);
+        System.out.println(parcela3);
 
         
     }
