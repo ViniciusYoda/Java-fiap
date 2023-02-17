3a9cf5dcbabc67ce8edfc69bf87a304866a6c75a
Vinícius Yoda <vyoda4604@gmail.com>
2023-02-17 11:07:49 -0300

    j

--- 2 - ano/regra de negocio/src/App.java
+++ 2 - ano/regra de negocio/src/App.java
@@ -1,7 +1,14 @@
+import java.util.Date;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner number  = new Scanner(system)
+        int contract = 0;
+        Date data;
+        Scanner number  = new Scanner(System.in);
+        System.out.println("Enter contract data: ");
+        contract = number.nextInt();
+        System.out.println("Date (dd/mm/yyyy): ");
+        data = number
     }
 }
